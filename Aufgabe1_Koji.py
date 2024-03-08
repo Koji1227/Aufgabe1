58cb8555a865280177cb46e718c77bf449b856c6
Koji Okumura <2160611h@gsuite.kobe-u.ac.jp>
2024-03-08 14:28:55 +0100

    Second commit

---
+++ Aufgabe1_Koji.py
@@ -0,0 +1,20 @@
+# Programmieren vertieft
+# Aufgabe 1; 07.03.2024
+# Name: Koji Okumura
+
+import csv
+
+result = []
+
+with open('cities.csv', 'r', encoding = 'utf-8', newline = '') as f_r:
+    reader = csv.reader(f_r)
+    for row in reader:
+        if row[0].startswith('A') and int(row[3]) > 10000:
+            result.append(row)
+
+sorted_result = sorted(result, key = lambda x: (x[2], x[0]))
+
+with open('results.csv', 'w', encoding = 'utf-8', newline = '') as f_w:
+    writer = csv.writer(f_w)
+    writer.writerow(['Stadt', 'Bezirk', 'Bundesland', 'Einwohner'])
+    writer.writerows(sorted_result)
