a5d00ea6f6033bc1f6e63ff87341c289a9a1ce00
Gunter Vasold <gunter.vasold@uni-graz.at>
2024-03-14 13:10:06 +0100

    Add feedback for Aufgabe 1.1

--- Aufgabe1_Koji.py
+++ Aufgabe1_Koji.py
@@ -6,15 +6,19 @@ import csv
 
 result = []
 
+## newline = '' does not make sense here
 with open('cities.csv', 'r', encoding = 'utf-8', newline = '') as f_r:
     reader = csv.reader(f_r)
     header = next(reader) # Skip the header
     for row in reader:
+        ## This would miss all Cities with 'A' as first letter
+        ## Better: 'a' in row[0].lower()
         if ('a' in row[0]) and (int(row[3]) > 10000):
             result.append(row)
 
 sorted_result = sorted(result, key = lambda x: (x[2], x[0]))
 
+## newline = '' does not make sense here 
 with open('results.csv', 'w', encoding = 'utf-8', newline = '') as f_w:
     writer = csv.writer(f_w)
     writer.writerow(['Stadt', 'Bezirk', 'Bundesland', 'Einwohner'])
